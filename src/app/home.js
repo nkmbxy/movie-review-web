db9abfafc0bea499cad661922ed23c5ea89f65c3
Sasima Phanta <jassm10phanta@gmail.com>
2024-05-05 16:01:15 +0700

    update home.js

--- src/app/home.js
+++ src/app/home.js
@@ -1,5 +1,40 @@
-import { Grid, TextField, Button } from "@mui/material";
+import { Grid, TextField, Button, Box, Typography } from "@mui/material";
 
+const threeTopMovie = [ //mock data
+    {
+        img: 'https://freakingeek.com/wp-content/uploads/2023/04/Queenmaker-Banniere.jpg',
+    },
+    {
+        img: 'https://puui.wetvinfo.com/vcover_hz_pic/0/gnwjazjgmg997xg1607677060480/0',
+    },
+    {
+        img: 'https://image.tmdb.org/t/p/original/jOpb4ZMF9WyE1YPJfMfhonKGJzH.jpg',
+    }
+];
+
+const moviesList = [
+  {
+    img: 'https://freakingeek.com/wp-content/uploads/2023/04/Queenmaker-Banniere.jpg',
+  },
+  {
+    img: 'https://puui.wetvinfo.com/vcover_hz_pic/0/gnwjazjgmg997xg1607677060480/0',
+  },
+  {
+    img: 'https://image.tmdb.org/t/p/original/jOpb4ZMF9WyE1YPJfMfhonKGJzH.jpg',
+  },
+  {
+    img: 'https://freakingeek.com/wp-content/uploads/2023/04/Queenmaker-Banniere.jpg',
+  },
+  {
+    img: 'https://puui.wetvinfo.com/vcover_hz_pic/0/gnwjazjgmg997xg1607677060480/0',
+  },
+  {
+    img: 'https://image.tmdb.org/t/p/original/jOpb4ZMF9WyE1YPJfMfhonKGJzH.jpg',
+  },
+  {
+    img: 'https://freakingeek.com/wp-content/uploads/2023/04/Queenmaker-Banniere.jpg',
+  }
+]
 
 export default function HomePage() {
   return (
@@ -46,7 +81,44 @@ export default function HomePage() {
             </Button>
           </Grid>
         </Grid>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+          {threeTopMovie.map((item) => (
+            <Box key={item.img} sx={{ width: 400, height: 300, margin: 1,transition: "transform 0.3s ease",
+            "&:hover": {
+              transform: "scale(1.1)",}}}>
+                <img
+                    src={`${item.img}`}
+                    style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+                />
+            </Box>
+          ))}
+        </Box>
+        <Box sx={{ width: '100%', maxWidth: 500 }}>
+          <Typography
+            variant="h6" 
+              sx={{
+                  ml: 5,
+                  marginTop:4,
+                  fontWeight: 700,
+                  letterSpacing: ".1rem",
+                  textDecoration: "none",
+                }}>
+              Made in Chinese 
+          </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
+              {moviesList.map((item) => (
+                  <Box key={item.img} sx={{ width: 200, height: 200, margin: 1}}>
+                      <img
+                          src={`${item.img}`}
+                          style={{ width: '100%', height: '100%', objectFit: 'contain' }}
+                      />
+                  </Box>
+              ))}
+          </Box>
       </Grid>
     </Grid>
   );
 }
+
+// 'Chinese', 'England', 'Japan', 'Korea', 'Thai'
